f895548fe8c84e57e0678c59f7294bde850f2053
paddy sherry <hello@paddysherry.com>
2025-05-18 16:36:20 +0200

    asdf

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:minutly --force')->everyMinute();
+Schedule::command('app:minutly')->everyMinute();
