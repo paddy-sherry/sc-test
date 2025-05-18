034355e818fe247e862ddf8172d468540dc7e8ce
paddy sherry <hello@paddysherry.com>
2025-05-18 16:29:21 +0200

    adf

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\minutly;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('app:minutly --force')->daily();
